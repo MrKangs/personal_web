dd13573ec5a578cf781a8c6d5432df3f24c6579c
Mr.Kangs <gykang00@gmail.com>
2020-10-06 00:13:02 -0700

    Add some pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
+import {Layout, Header, Navigation, Content, MDLComponent } from 'react-mdl';
+import Main from './components/main';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <div style={{height: '300px', position: 'relative'}}>
+    <Layout style={{background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover'}}>
+        <Header transparent title="Title" style={{color: 'white'}}>
+            <Navigation>
+                <Link to="/resume">Resume</Link>
+                <Link to="/aboutme">About Me</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
+            </Navigation>
+        </Header>
+          <Content>
+            <Main/>
+          </Content>
+    </Layout>
+</div>
   );
 }
 
