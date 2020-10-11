e178dc88c242850cfd063ba4c3e2c34b21c2c2e0
Mr.Kangs <gykang00@gmail.com>
2020-10-11 01:47:46 -0700

    Updates

---
+++ precache-manifest.0dfb29688c92dee1d8529664110b5aa0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9d07b14995bf54ea1ef60002772f8c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90fa38df9828935a0922",
+    "url": "/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "4977df4a8aa1b7ac005d",
+    "url": "/static/css/main.4c5ee42f.chunk.css"
+  },
+  {
+    "revision": "90fa38df9828935a0922",
+    "url": "/static/js/2.d22d8bb8.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.d22d8bb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4977df4a8aa1b7ac005d",
+    "url": "/static/js/main.f631d3c7.chunk.js"
+  },
+  {
+    "revision": "cf0198089fe768059223",
+    "url": "/static/js/runtime-main.cf3b6b84.js"
+  }
+]);
