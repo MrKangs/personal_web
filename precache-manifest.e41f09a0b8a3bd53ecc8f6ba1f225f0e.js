9da50bc74edb0dfbb02288a4e064c1946efbdd97
MrKangs <MrKangs@users.noreply.github.com>
2020-10-17 09:14:32 +0000

    deploy: d75cb2f26cf0494a1c2d96971b66bcf25542d828

---
+++ precache-manifest.e41f09a0b8a3bd53ecc8f6ba1f225f0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f18c25caae092f5a1a8f5e75031f29fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7300607048a8b9de8896",
+    "url": "/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "60ead19e27e2e27cab17",
+    "url": "/static/css/main.666f8401.chunk.css"
+  },
+  {
+    "revision": "7300607048a8b9de8896",
+    "url": "/static/js/2.b0f3a0ef.chunk.js"
+  },
+  {
+    "revision": "8e5ae93e18f66c1d607de05f5b5d011b",
+    "url": "/static/js/2.b0f3a0ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60ead19e27e2e27cab17",
+    "url": "/static/js/main.62a61217.chunk.js"
+  },
+  {
+    "revision": "cf0198089fe768059223",
+    "url": "/static/js/runtime-main.cf3b6b84.js"
+  }
+]);
